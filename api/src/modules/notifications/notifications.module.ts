09df0be1efaa699e426e91069e6da06cca6f026a
Timilehin Toba <76178289+SirTimi@users.noreply.github.com>
2025-12-14 01:00:24 +0100

    auth

--- api/src/modules/notifications/notifications.module.ts
+++ api/src/modules/notifications/notifications.module.ts
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { PrismaService } from '../../prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   providers: [NotificationsService, PrismaService],
   controllers: [NotificationsController],
   exports: [NotificationsService],
